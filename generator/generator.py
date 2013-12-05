51ce9af69a6ca66cf431616a74cbfe833bfef6c7
Alexey Bulatov <AlexeyABulatov@yandex.ru>
2013-12-05 13:24:17 +0400

    Initial commit

---
+++ generator/generator.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python2.7
+
+print "Hello, here will be code generator!"
---
+++ sample/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Here will be code sample" << std::endl;
+    return 0; 
+}
